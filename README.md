8fa400429c858e20a1e20bd01633b7317b95c0f0
Christi Richards <christi.richards@gmail.com>
2017-03-01 18:25:12 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# N1 Github
+
+### The Github info viewer for Nylas Mail and Nylas N1/Pro
+This is an improved version of the [View on Github](https://github.com/ForbesLindesay/N1-GitHub) plugin, extended by [@ForbesLindesay](https://github.com/ForbesLindesay) which shows both whether the e-mail is about an issue, pull request, or something else, and whether the issue or pull request is closed.
+
+The updated version provides cohesion in design when using labels to organize mail and reduces the button size to look more like the default N1 theme.
+
+### Version
+1.0.0
+
+### License
+GNU GPLv3
+
+### Updated
+March 1st, 2017
+
+## How to install
+
+1. Download and unzip `n1-github.zip` from the [Releases page](https://github.com/christirichards/n1-github/releases/latest)
+
+2. In Nylas Mail, Select the menu bar option `Developer > Install a Plugin...`, then select the unzipped folder, `n1-github`
+
+3. You should now see the plugin in the plugins pane in Nylas Mail (`Preferences > Plugins`)
+
+## Reporting Bugs
+
+- **Feature Requests or Bug Reports**: Submit them through the [issues](issues) pane.
+
+## Questions & Comments
+
+Any questions or comments can be directed to Christi Richards at: [christi@christirichards.com](mailto:christi@christirichards.com)
---
+++ lib/github-status.jsx
@@ -0,0 +1,149 @@
+import {shell} from 'electron'
+import {Actions, React, DatabaseStore, Message} from 'nylas-exports'
+import {RetinaImg, KeyCommandsRegion} from 'nylas-component-kit'
+import github from 'github-basic';
+import {findGitHubLink, parseLink, isRelevantThread} from './utils';
+
+import GitHubPreferencesStore from './github-preferences-store';
+
+function loadMessages(thread) {
+  // TODO: try using findBy to return just one message
+  const query = DatabaseStore.findAll(Message);
+  query.where({threadId: thread.id});
+  query.include(Message.attributes.body);
+  return query.then(messages => {
+    for (let i = 0; i < messages.length; i++) {
+      const link = findGitHubLink(messages[i]);
+      if (link) {
+        return link;
+      }
+    }
+    return null;
+  });
+}
+export default class GitHubStatus extends React.Component {
+  static displayName = "GitHubStatus"
+
+  static propTypes: {
+    thread: React.PropTypes.object,
+  };
+
+  constructor(props) {
+    super(props)
+    this.state = {};
+  }
+
+  componentDidMount() {
+    this.setState(this._getStateFromProps(this.props));
+  }
+  componentWillReceiveProps(props) {
+    if (props.thread) {
+      this.setState(this._getStateFromProps(props));
+    }
+  }
+
+  _getStateFromProps(props) {
+    if (this._threadID && props.thread.id === this._threadID) {
+      return this.state;
+    }
+    this._threadID = props.thread.id;
+    if (isRelevantThread(props.thread.participants)) {
+      const threadID = props.thread.id;
+      loadMessages(props.thread).then(link => {
+        if (this.props.thread && this.props.thread.id === threadID) {
+          this.setState(this._getStateFromLink(link));
+        }
+      }).catch(ex => {
+        setTimeout(() => { throw ex; }, 0);
+      });
+    }
+    return {link: null, info: null, status: null};
+  }
+  _getStateFromLink(link) {
+    if (this.state && this.state.link === link) {
+      return this.state;
+    }
+    const info = parseLink(link);
+    if (info) {
+      // Intentionally not subscribing to the access token
+      const accessToken = GitHubPreferencesStore.accessToken();
+      const client = github({version: 3, auth: accessToken});
+      client.get('/repos/:owner/:repo/issues/:number', {
+        owner: info.owner,
+        repo: info.repo,
+        number: info.number,
+      }).done(result => {
+        if (this.state.link === link) {
+          this.setState({status: result});
+        }
+      });
+    }
+    return {link, info, status: null};
+  }
+
+  _openLink = () => {
+    Actions.recordUserEvent("Github Thread Opened", {pageUrl: this.state.link})
+    if (this.state.link) {
+      shell.openExternal(this.state.link)
+    }
+  }
+
+  render() {
+    if (!this.state.link) { return false }
+    return (
+      <div className="github-issue-status">
+        {this._renderStatus()}
+        <button
+          className="btn btn-toolbar btn-view-on-github"
+          onClick={this._openLink}
+          title={"Visit Thread on GitHub"}
+        >
+          <RetinaImg
+            mode={RetinaImg.Mode.ContentIsMask}
+            url="nylas://n1-github/assets/github@2x.png"
+          />
+          {this._renderType()}
+        </button>
+      </div>
+    )
+  }
+  _renderStatus() {
+    if (!this.state.info) {
+      return null;
+    }
+    const status = this.state.status;
+    if (!status) {
+      return (
+        <span className="btn btn-toolbar">
+          Loading...
+        </span>
+      );
+    }
+    switch (status.state) {
+      case 'open':
+        return (
+          <span className="btn btn-toolbar github-issue-status github-issue-status-open">
+            Open
+          </span>
+        );
+      case 'closed':
+        return (
+          <span className="btn btn-toolbar github-issue-status github-issue-status-closed">
+            Closed
+          </span>
+        );
+    }
+  }
+  _renderType() {
+    const info = this.state.info;
+    if (!info) {
+      return null;
+    }
+    switch (info.type) {
+      case 'issue':
+        return ' Issue';
+      case 'pull-request':
+        return ' Pull Request';
+    }
+  }
+}
